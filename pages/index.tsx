16e3533ddb3e6dc26506476e2ad5bf69cf78bbe7
Amiel Brencis Salipande <absalipande@gmail.com>
2023-04-18 17:22:45 +0800

    feat: added billboard component and random movie endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
-import { signOut } from 'next-auth/react';
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,10 +25,8 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   return (
     <>
-      <h1 className='text-blue-500 font-bold underline'>Netflix clone</h1>
-      <button className='h-10 w-full bg-white' onClick={() => signOut}>
-        Logout
-      </button>
+    <Navbar />
+    <Billboard />
     </>
   );
 }
