c8745b6273dc6b2b0d9a89a25fecc43debd8df12
Amiel Brencis Salipande <absalipande@gmail.com>
2023-04-19 14:07:07 +0800

    fixed: small changes

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,8 +2,10 @@ import React, { useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { PlayIcon } from '@heroicons/react/24/solid';
+
 import { MovieInterface } from '@/types';
 import FavoriteButton from '@/components/FavoriteButton';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
 
 interface MovieCardProps {
   data: MovieInterface;
@@ -11,7 +13,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
-//   const { openModal } = useInfoModalStore();
+  const { openModal } = useInfoModalStore();
 
   const redirectToWatch = useCallback(
     () => router.push(`/watch/${data.id}`),
@@ -26,37 +28,37 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         alt='Movie'
         draggable={false}
         className='
-          cursor-pointer
-          object-cover
-          transition
-          duration
-          shadow-xl
-          rounded-md
-          group-hover:opacity-90
-          sm:group-hover:opacity-0
-          delay-300
-          w-full
-          h-[12vw]
-        '
+        cursor-pointer
+        object-cover
+        transition
+        duration
+        shadow-xl
+        rounded-md
+        group-hover:opacity-90
+        sm:group-hover:opacity-0
+        delay-300
+        w-full
+        h-[12vw]
+      '
       />
       <div
         className='
-          opacity-0
-          absolute
-          top-0
-          transition
-          duration-200
-          z-10
-          invisible
-          sm:visible
-          delay-300
-          w-full
-          scale-0
-          group-hover:scale-110
-          group-hover:-translate-y-[6vw]
-          group-hover:translate-x-[2vw]
-          group-hover:opacity-100
-        '
+        opacity-0
+        absolute
+        top-0
+        transition
+        duration-200
+        z-10
+        invisible
+        sm:visible
+        delay-300
+        w-full
+        scale-0
+        group-hover:scale-110
+        group-hover:-translate-y-[6vw]
+        group-hover:translate-x-[2vw]
+        group-hover:opacity-100
+      '
       >
         <img
           onClick={redirectToWatch}
@@ -64,28 +66,28 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           alt='Movie'
           draggable={false}
           className='
-            cursor-pointer
-            object-cover
-            transition
-            duration
-            shadow-xl
-            rounded-t-md
-            w-full
-            h-[12vw]
-          '
+          cursor-pointer
+          object-cover
+          transition
+          duration
+          shadow-xl
+          rounded-t-md
+          w-full
+          h-[12vw]
+        '
         />
         <div
           className='
-            z-10
-            bg-zinc-800
-            p-2
-            lg:p-4
-            absolute
-            w-full
-            transition
-            shadow-md
-            rounded-b-md
-            '
+          z-10
+          bg-zinc-800
+          p-2
+          lg:p-4
+          absolute
+          w-full
+          transition
+          shadow-md
+          rounded-b-md
+          '
         >
           <div className='flex flex-row items-center gap-3'>
             <div
@@ -96,7 +98,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             </div>
             <FavoriteButton movieId={data.id} />
             <div
-              // onClick={() => openModal(data?.id)}
+              onClick={() => openModal(data?.id)}
               className='cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'
             >
               <ChevronDownIcon className='text-white group-hover/item:text-neutral-300 w-4 lg:w-6' />
